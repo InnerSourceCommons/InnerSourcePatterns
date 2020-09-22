726b7815c1317d5b07a5f7ca05f6294136fa7cad
Sebastian Spier <github@spier.hu>
2020-09-22 07:26:47 +0200

    add script to generate an overview of all 2-structured patterns. preparation for automatic toc generation in the book

---
+++ book/generate_toc.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+
+require 'pp'
+
+# Extracts the value of the title section from a markdown file
+
+def extract_title(file)
+  markdown = open(file).readlines().join
+  doc = CommonMarker.render_doc(markdown)
+
+  title_found = false
+  title_nodes = []
+
+  # Capitalize all regular text in headers
+  doc.walk do |node|
+    if node.type == :header
+      node.each do |subnode|
+        if subnode.type == :text and subnode.string_content == "Title"
+          title_found = true
+        end
+      end
+    elsif node.type == :paragraph && title_found == true
+      node.each do |subnode|
+        if subnode.type == :text
+          title_nodes << subnode.string_content
+        end
+      end
+      break
+    end
+  end
+
+  # pp title_nodes
+
+  title = title_nodes.join
+  return title
+
+end
+
+
+
+patterns = Dir["../patterns/2-structured/*.md","../patterns/2-structured/project-setup/*.md"]
+
+patterns.each do |file|
+  title = extract_title(file)
+  puts "[#{title}](#{file})"
+end
